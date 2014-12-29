da9277b124fdc4520b17c4e26e70dc4a3392be3c
Sputnik Antolovich <inquiry@dermah.com>
2014-12-29 22:39:31 +1100

    Add failing tests

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var expect = require('expect.js');
+
+describe ('Transmitter', function() {
+  describe ('#transmit()', function() {
+    it ('should transmit information via UDP', function () {
+      expect().fail();
+    });
+
+    it ('should transmit a Pulsar packet when given some valid JSON', function () {
+      expect().fail();
+    });
+
+    it ('should transmit packets to port 6660', function () {
+      expect().fail();
+    });
+
+  })
+})
