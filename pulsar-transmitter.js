c6fa918d324a9b61d9f7d075d7a04240daeec9aa
Sputnik Antolovich <inquiry@dermah.com>
2015-01-06 23:32:35 +1100

    Pass basic transmission tests

--- pulsar-transmitter.js
+++ pulsar-transmitter.js
@@ -1,7 +1,16 @@
+var dgram = require('dgram');
+
 var Transmitter = function () {};
 
 Transmitter.prototype.transmit = function (packet) {
-
+  var client = dgram.createSocket("udp4");
+  packet = new Buffer(JSON.stringify(packet));
+  client.bind(function() {
+    client.setBroadcast(true);
+    client.send(packet, 0, packet.length, 6660, "255.255.255.255", function(err, bytes) {
+      console.log(packet);
+    });
+  });
 }
 
 module.exports = Transmitter;
